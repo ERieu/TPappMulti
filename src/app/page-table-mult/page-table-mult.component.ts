7e9d0d3327d846e07af4392604caaad5d508345d
ERieu <evan.rieu@gmail.com>
2024-12-10 12:11:21 +0100

    1er commit

---
+++ src/app/page-table-mult/page-table-mult.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-table-mult',
+  templateUrl: './page-table-mult.component.html',
+  styleUrls: ['./page-table-mult.component.css']
+})
+
+export class PageTableMultComponent {}
